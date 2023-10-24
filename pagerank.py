196360f67fc1827a73a23660595378e6f85dc487
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-10-24 16:56:33 +0200

    Reformat code

--- pagerank.py
+++ pagerank.py
@@ -1,24 +1,9 @@
 import os
-import random
 import re
 import sys
 
 DAMPING = 0.85
 SAMPLES = 10000
-
-
-def main():
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python pagerank.py corpus")
-    corpus = crawl(sys.argv[1])
-    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
-    print(f"PageRank Results from Sampling (n = {SAMPLES})")
-    for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
-    ranks = iterate_pagerank(corpus, DAMPING)
-    print(f"PageRank Results from Iteration")
-    for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
 
 
 def crawl(directory):
@@ -85,4 +70,18 @@ def iterate_pagerank(corpus, damping_factor):
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
